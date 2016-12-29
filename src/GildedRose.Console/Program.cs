56369df8605e803f67ad261fe97dd8a03ad6404b
Sebastian Morales <semora@microsoft.com>
2016-12-29 14:02:24 -0800

    Create properties for each of the existing items

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -5,33 +5,37 @@ namespace GildedRose.Console
     public class Program
     {
         public IList<Item> Items;
+
+        public static Item DexteryVest => new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20};
+        public static Item AgedBrie => new Item {Name = "Aged Brie", SellIn = 2, Quality = 0};
+        public static Item MongooseElixir => new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7};
+        public static Item Sulfuras => new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80};
+        public static Item BackstagePasses => new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20};
+        public static Item ManaCake => new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6};
+
         public static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
+            var items = new List<Item>
+            {
+                DexteryVest,
+                AgedBrie,
+                MongooseElixir,
+                Sulfuras,
+                BackstagePasses,
+                ManaCake
+            };
+
             var app = new Program()
             {
-                Items = new List<Item>
-                                          {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          }
+                Items = items
 
             };
 
             app.UpdateQuality();
 
             System.Console.ReadKey();
-
         }
 
         public void UpdateQuality()
