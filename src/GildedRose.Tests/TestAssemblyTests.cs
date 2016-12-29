b8d2a1be704f4a9f18704377af2d8bd6925c79b1
Sebastian Morales <semora@microsoft.com>
2016-12-29 14:47:16 -0800

    test for aged brie

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -20,9 +20,6 @@ namespace GildedRose.Tests
         [Fact]
         public void TestDexteryVest()
         {
-            var baseDexteryVest = Program.DexteryVest;
-            var dexteryVest = Program.DexteryVest;
-            var app = new Program(new List<Item> { dexteryVest });
             var expectedValue = new List<ExpectedValues>
             {
                 new ExpectedValues { Quality = 19, SellIn = 9 },
@@ -54,17 +51,57 @@ namespace GildedRose.Tests
                 new ExpectedValues { Quality = 0, SellIn = -17 },
                 new ExpectedValues { Quality = 0, SellIn = -18 },
                 new ExpectedValues { Quality = 0, SellIn = -19 },
-                new ExpectedValues { Quality = 0, SellIn = -20 },
+                new ExpectedValues { Quality = 0, SellIn = -20 }
             };
 
+            RunUpdatesAndAssert(Program.DexteryVest, expectedValue);
+        }
+
+        [Fact]
+        public void TestAgedBrie()
+        {
+            var expectedValues = new List<ExpectedValues>
+            {
+                new ExpectedValues { Quality = 1, SellIn = 1 },
+                new ExpectedValues { Quality = 2, SellIn = 0 },
+                new ExpectedValues { Quality = 4, SellIn = -1 },
+                new ExpectedValues { Quality = 6, SellIn = -2 },
+                new ExpectedValues { Quality = 8, SellIn = -3 },
+                new ExpectedValues { Quality = 10, SellIn = -4 },
+                new ExpectedValues { Quality = 12, SellIn = -5 },
+                new ExpectedValues { Quality = 14, SellIn = -6 },
+                new ExpectedValues { Quality = 16, SellIn = -7 },
+                new ExpectedValues { Quality = 18, SellIn = -8 },
+                new ExpectedValues { Quality = 20, SellIn = -9 },
+                new ExpectedValues { Quality = 22, SellIn = -10 },
+                new ExpectedValues { Quality = 24, SellIn = -11 },
+                new ExpectedValues { Quality = 26, SellIn = -12 },
+                new ExpectedValues { Quality = 28, SellIn = -13 },
+                new ExpectedValues { Quality = 30, SellIn = -14 },
+                new ExpectedValues { Quality = 32, SellIn = -15 },
+                new ExpectedValues { Quality = 34, SellIn = -16 },
+                new ExpectedValues { Quality = 36, SellIn = -17 },
+                new ExpectedValues { Quality = 38, SellIn = -18 },
+                new ExpectedValues { Quality = 40, SellIn = -19 },
+                new ExpectedValues { Quality = 42, SellIn = -20 },
+            };
+
+            RunUpdatesAndAssert(Program.AgedBrie, expectedValues);
+        }
+
+        private static void RunUpdatesAndAssert(Item updatee, IEnumerable<ExpectedValues> values)
+        {
+            var expectedName = updatee.Name;
+            var app = new Program(new List<Item> { updatee });
             var updateStep = 1;
-            foreach (var expected in expectedValue)
+
+            foreach (var expected in values)
             {
                 app.UpdateQuality();
 
-                dexteryVest.Name.ShouldBeEquivalentTo(baseDexteryVest.Name, $"<Name on update step [{updateStep}]>");
-                dexteryVest.Quality.ShouldBeEquivalentTo(expected.Quality, $"<Quality on update step [{updateStep}]?");
-                dexteryVest.SellIn.ShouldBeEquivalentTo(expected.SellIn, $"<SellIn on update step [{updateStep}]>");
+                updatee.Name.ShouldBeEquivalentTo(expectedName, $"<Name on update step [{updateStep}]>");
+                updatee.Quality.ShouldBeEquivalentTo(expected.Quality, $"<Quality on update step [{updateStep}]?");
+                updatee.SellIn.ShouldBeEquivalentTo(expected.SellIn, $"<SellIn on update step [{updateStep}]>");
 
                 ++updateStep;
             }
