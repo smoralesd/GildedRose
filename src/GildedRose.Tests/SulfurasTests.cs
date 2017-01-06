bfe3a3f59597d96a95149eb6844d0eb7e281740b
Sebastian Morales <semora@microsoft.com>
2017-01-05 16:37:16 -0800

    Add tests for Sulfuras

---
+++ src/GildedRose.Tests/SulfurasTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using GildedRose.Console;
+using Xunit;
+
+namespace GildedRose.Tests
+{
+    public class SulfurasTests
+    {
+        [Fact]
+        public void RandomValuesTests()
+        {
+            var generator = new Random();
+            for (var cycle = 0; cycle < 100; ++cycle)
+            {
+                var sellIn = generator.Next(-10, 10);
+                var quality = generator.Next(0, 50);
+                var sulfuras = new Item
+                {
+                    Name = "Sulfuras, Hand of Ragnaros",
+                    SellIn = sellIn,
+                    Quality = quality
+                };
+
+                new TestRunner(sulfuras)
+                    .SetExpectedSellIn(sellIn)
+                    .SetExpectedQuality(quality)
+                    .Run();
+            }
+        }
+    }
+}
