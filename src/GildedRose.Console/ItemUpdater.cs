53d8ebfc1ee383e891e3f0abd099d845b511bfc5
Sebastian Morales <semora@microsoft.com>
2017-01-06 15:30:18 -0800

    Move updater functions inside the item updater factory, make the class private

--- src/GildedRose.Console/ItemUpdater.cs
+++ src/GildedRose.Console/ItemUpdater.cs
@@ -21,6 +21,48 @@ namespace GildedRose.Console
                     return new UpdateByFunction(item, toUpdate => { });
             }
         }
+
+        private static class ItemUpdater
+        {
+            public static void DecreaseByAmount(Item item, int amount)
+            {
+                var effectiveAmount = item.SellIn <= 0 ? 2 * amount : amount;
+                item.Quality = Math.Max(item.Quality - effectiveAmount, 0);
+                --item.SellIn;
+            }
+
+            public static void IncreaseByAmount(Item item, int amount)
+            {
+                var effectiveAMount = item.SellIn <= 0 ? 2 * amount : amount;
+                item.Quality = Math.Min(item.Quality + effectiveAMount, 50);
+                --item.SellIn;
+            }
+
+            public static void BackstagePasses(Item item)
+            {
+                ++item.Quality;
+
+                if (item.SellIn < 11)
+                {
+                    ++item.Quality;
+                }
+
+                if (item.SellIn < 6)
+                {
+                    ++item.Quality;
+                }
+
+                item.Quality = Math.Min(item.Quality, 50);
+                --item.SellIn;
+
+                if (item.SellIn < 0)
+                {
+                    item.Quality = 0;
+                }
+            }
+        }
+
+
     }
 
     public interface IItemUpdater
@@ -42,80 +84,6 @@ namespace GildedRose.Console
         public void Update()
         {
             _updateFunction(_item);
-        }
-    }
-
-    public class ItemUpdater : IItemUpdater
-    {
-        private readonly Item _item;
-
-        public ItemUpdater(Item item)
-        {
-            _item = item;
-        }
-
-        public void Update()
-        {
-            UpdateItemQuality(_item);
-        }
-
-        public static void DecreaseByAmount(Item item, int amount)
-        {
-            var effectiveAmount = item.SellIn <= 0 ? 2 * amount : amount;
-            item.Quality = Math.Max(item.Quality - effectiveAmount, 0);
-            --item.SellIn;
-        }
-
-        public static void IncreaseByAmount(Item item, int amount)
-        {
-            var effectiveAMount = item.SellIn <= 0 ? 2*amount : amount;
-            item.Quality = Math.Min(item.Quality + effectiveAMount, 50);
-            --item.SellIn;
-        }
-
-        public static void BackstagePasses(Item item)
-        {
-            ++item.Quality;
-
-            if (item.SellIn < 11)
-            {
-                ++item.Quality;
-            }
-
-            if (item.SellIn < 6)
-            {
-                ++item.Quality;
-            }
-
-            item.Quality = Math.Min(item.Quality, 50);
-            --item.SellIn;
-
-            if (item.SellIn < 0)
-            {
-                item.Quality = 0;
-            }
-        }
-
-        public static void UpdateItemQuality(Item item)
-        {
-            switch (item.Name)
-            {
-                case "Conjured Mana Cake":
-                    DecreaseByAmount(item, 2);
-                    return;
-                case "+5 Dexterity Vest":
-                case "Elixir of the Mongoose":
-                    DecreaseByAmount(item, 1);
-                    return;
-                case "Aged Brie":
-                    IncreaseByAmount(item, 1);
-                    return;
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    BackstagePasses(item);
-                    return;
-                default:
-                    return;
-            }
         }
     }
 }
